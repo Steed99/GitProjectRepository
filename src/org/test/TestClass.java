323c9a73b7ef4671f17af4702c6ff73dbd6d35e0
Steed99 <chynechandran@gmail.com>
2021-09-13 18:57:02 +0530

    sample code added

---
+++ src/org/test/TestClass.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(1);
+System.out.println(11);
+System.out.println(111);
+System.out.println(1111);
+
+	}
+
+}
